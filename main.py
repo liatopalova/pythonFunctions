b5ec52e4cfc01c09ab8c599c788a4f8f3f9f11a5
Lia Topalova <lia.topalova@gmail.com>
2023-10-07 17:37:18 +0300

    Implemented first task

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 
+my_list = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10, 11]
+
+
+def mult_nums(list_1):
+    num = 1
+    for number in list_1:
+        num *= number
+    return num
+
+
+print(f"Mult_nums: {mult_nums(my_list)}")
